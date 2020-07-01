f47ad59e9fdbd12c6baaec593a2f210478daf378
Isadora Dantas <55112637+devisaah@users.noreply.github.com>
2020-07-01 09:25:26 -0300

    Update controller.rb

--- lib/templates/rails/scaffold_controller/controller.rb
+++ lib/templates/rails/scaffold_controller/controller.rb
@@ -92,7 +92,7 @@ class <%= controller_class_name %>Controller < ApplicationController
   
     def search
       respond_to do |format|
-        format.json { render json: <%= singular_table_name %>.search(params[:search])  }
+        format.json { render json: <%= singular_table_name.capitalize %>.search(params[:search])  }
       end
     end
   
